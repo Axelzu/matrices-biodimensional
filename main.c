cdf403a186bcf2a14491a16ded1e65620ef8a6df
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-11-21 15:05:49 -0500

    3

--- main.c
+++ main.c
@@ -8,6 +8,20 @@ int main(int argc, char const *argv[])
     printf("Ingrese el valor para columnas: ");
     scanf("%d", &m);
     int matrix[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (i == j)
+            {
+                matrix[i][j] = 1;
+            }
+            else
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
 
     return 0;
 }
