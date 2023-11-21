2614c4bbc2689883e183408c67b685bb5e3e2201
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-11-21 15:03:53 -0500

    2

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 
 int main(int argc, char const *argv[])
 {
-    
+    int n, m;
+    printf("Ingrese el valor para filas: ");
+    scanf("%d", &n);
+    printf("Ingrese el valor para columnas: ");
+    scanf("%d", &m);
+    int matrix[n][m];
 
     return 0;
 }
